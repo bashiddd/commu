9a59f7fc6e8a7a2f6d405646578cf4f412982d74
bashi <bashi57.d@gmail.com>
2022-03-29 16:56:56 +0900

    g model User

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_27_171331) do
+ActiveRecord::Schema.define(version: 2022_03_29_074849) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
