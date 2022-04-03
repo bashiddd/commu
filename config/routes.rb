701b036d73eaaa98243563576e8f9c7e4c377974
bashi <bashi57.d@gmail.com>
2022-04-03 22:48:22 +0900

    header-menus

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/' => "home#top"
   get "commu/top" => "commu#top"
   get "signup" => "users#new"
   post "users/create" => "users#create"
@@ -6,6 +7,5 @@ Rails.application.routes.draw do
   post "logout" => "users#logout"
   # get 'users/:id' => "users#show"
   # get 'posts/index'
-  get '/' => "home#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
