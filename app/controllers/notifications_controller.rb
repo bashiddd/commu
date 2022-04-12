bc6336bb733ca10561010737ea491e9373ea05f4
bashi <bashi57.d@gmail.com>
2022-04-12 23:41:07 +0900

    SystemStackError (stack level too deep):

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,17 @@
 class NotificationsController < ApplicationController
   def index
   end
+
+  def request
+    commu = Community.find_by(id: params[:id])
+    notification = Notification.new(
+      receiver_id: commu.created_by,
+      sender_id: @current_user.id,
+      commu_id: commu.id
+    )
+    notification.save
+    redirect_to("/commu/top")
+    
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  get 'notifications/index'
   get '/' => "home#top"
   get "commu/top" => "commu#top"
-
+  
   get "signup" => "users#new"
   post "users/create" => "users#create"
   post "login" => "users#login"
   post "logout" => "users#logout"
-
+  
   post 'users/:id/update' => "users#update"
   get 'users/:id/edit' => "users#edit"
   get 'users/:id' => "users#show"
@@ -16,8 +15,11 @@ Rails.application.routes.draw do
   post "commu/create" => "commu#create"
   get "commu/narrow_down" => "commu#narrow_down"
   get "commu/:id" => "commu#show"
-
+  
   get "members/:id" => "members#index"
+
+  get 'notifications/index'
+  get "request/:id" => "notifications#request"
   # get 'posts/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
