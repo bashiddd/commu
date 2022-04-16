c2650f17d9008dcf783e903951462d34d13a7c63
bashi <bashi57.d@gmail.com>
2022-04-16 18:31:34 +0900

    commu validates

--- app/controllers/commu_controller.rb
+++ app/controllers/commu_controller.rb
@@ -41,28 +41,29 @@ class CommuController < ApplicationController
     )
     
     if @commu.save
+      @commu.image_name1 = "#{@commu.id}.jpg"
+      image1 = params[:image1]
+      File.binwrite("public/commu_images1/#{@commu.image_name1}", image1.read)
+      @commu.image_name2 = "#{@commu.id}.jpg"
+      image2 = params[:image2]
+      File.binwrite("public/commu_images2/#{@commu.image_name2}", image2.read)
+      @commu.image_name3 = "#{@commu.id}.jpg"
+      image3 = params[:image3]
+      File.binwrite("public/commu_images3/#{@commu.image_name3}", image3.read)
+
       flash[:notice] = "コミュニティの作成が完了しました"
       redirect_to("/commu/#{@commu.id}")
     else
+      flash[:notice] = "全て入力してください"
       render("commu/new")
     end
 
-    @commu.image_name1 = "#{@commu.id}.jpg"
-    image1 = params[:image1]
-    File.binwrite("public/commu_images1/#{@commu.image_name1}", image1.read)
-
-    @commu.image_name2 = "#{@commu.id}.jpg"
-    image2 = params[:image2]
-    File.binwrite("public/commu_images2/#{@commu.image_name2}", image2.read)
-    
-    @commu.image_name3 = "#{@commu.id}.jpg"
-    image3 = params[:image3]
-    File.binwrite("public/commu_images3/#{@commu.image_name3}", image3.read)
-    
     @commu.save
     
-    @member = Member.new(commu_id: @commu.id, user_id: @current_user.id)
-    @member.save
+    if @commu.save
+      @member = Member.new(commu_id: @commu.id, user_id: @current_user.id)
+      @member.save
+    end
   end
 
   def show
--- app/models/community.rb
+++ app/models/community.rb
@@ -1,2 +1,10 @@
 class Community < ApplicationRecord
+    validates :name, {presence: true}
+    validates :genre, {presence: true}
+    validates :area, {presence: true}
+    validates :detail, {presence: true}
+    validates :image_name1, {presence: true}
+    validates :image_name2, {presence: true}
+    validates :image_name3, {presence: true}
+    validates :overview, {presence: true}
 end
