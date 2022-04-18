0fa4293af04354242e98e3ddb0e3cbec915c3a15
bashi <bashi57.d@gmail.com>
2022-04-18 20:03:18 +0900

    add_link_to_users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_17_132910) do
+ActiveRecord::Schema.define(version: 2022_04_18_105512) do
 
   create_table "communities", force: :cascade do |t|
     t.string "name"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2022_04_17_132910) do
     t.datetime "updated_at", null: false
     t.string "image_name"
     t.text "bio"
+    t.text "link"
   end
 
 end
