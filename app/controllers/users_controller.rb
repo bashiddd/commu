538e35fd4af524687e29bc743133f8b209f5817a
bashi <bashi57.d@gmail.com>
2022-04-22 23:36:59 +0900

    validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   # before_action :authenticate_user, {only: [:show]}
-  before_action :forbid_login_user, {only: [:login]}
-  # before_action :ensure_correct_user, {only: [:show]}
+  before_action :forbid_login_user, {only: [:login_reload, :new, :new_reload]}
+  before_action :ensure_correct_user, {only: [:edit, :update, :update_reload]}
   def new
     @user = User.new
   end
